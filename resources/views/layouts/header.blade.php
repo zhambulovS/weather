34043e9b9acb9b22a1a1b32abc2e8f72c7ca84bf
Sultanbek <zambulovsultanbek@gmail.com>
2024-08-06 16:12:00 +0500

    add main icon

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,5 +4,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{asset('css/app.css')}}">
+    <link rel="icon" href="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png" type="image/png">
     <title> Weather </title>
 </head>
